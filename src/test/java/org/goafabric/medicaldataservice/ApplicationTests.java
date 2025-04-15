0a1e3ae160df9a4c2823a2421a4dd6c2f9d08454
Andreas Mautsch <andreas.mautsch@cgm.com>
2025-04-15 15:22:00 +0200

    changes

--- src/test/java/org/goafabric/medicaldataservice/ApplicationTests.java
+++ src/test/java/org/goafabric/medicaldataservice/ApplicationTests.java
@@ -1,15 +1,13 @@
-package org.goafabric.medicaldataservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-    @Test
-    void contextLoads() {
-        // This test will start the Spring application context and then terminate
-
-    }
-
-
-}
+//package org.goafabric.medicaldataservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//    @Test
+//    void contextLoads() {
+//        // This test will start the Spring application context and then terminate
+//
+//    }
+//}
