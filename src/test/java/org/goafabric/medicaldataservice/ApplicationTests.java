c75769466f81ec28212f97911bf089c820cbbf50
Andreas Mautsch <amautsch>
2025-04-15 07:47:24 +0200

    initial import

---
+++ src/test/java/org/goafabric/medicaldataservice/ApplicationTests.java
@@ -0,0 +1,12 @@
+package org.goafabric.medicaldataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+        // This test will start the Spring application context and then terminate
+    }
+}
