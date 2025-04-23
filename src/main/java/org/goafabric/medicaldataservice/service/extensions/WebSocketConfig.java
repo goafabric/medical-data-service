2e2292337349185469fe0377532d3eb966aef02d
Andreas Mautsch <andreas.mautsch@cgm.com>
2025-04-23 16:28:52 +0200

    build added

--- src/main/java/org/goafabric/medicaldataservice/service/extensions/WebSocketConfig.java
+++ src/main/java/org/goafabric/medicaldataservice/service/extensions/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package org.goafabric.medicaldataservice.service.extensions;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -14,6 +15,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/websocket").withSockJS();
     }
 
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new SubscriptionInterceptor());
+    }
 
 
 }
