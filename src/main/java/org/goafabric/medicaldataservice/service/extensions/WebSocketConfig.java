704214bab0d9178e1d370ac9c4dcf115c16646c6
Andreas Mautsch <amautsch>
2025-04-28 14:20:14 +0200

    changes

--- src/main/java/org/goafabric/medicaldataservice/service/extensions/WebSocketConfig.java
+++ src/main/java/org/goafabric/medicaldataservice/service/extensions/WebSocketConfig.java
@@ -1,6 +1,10 @@
 package org.goafabric.medicaldataservice.service.extensions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -8,9 +12,13 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -18,7 +26,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket")
+                .addInterceptors(new CustomHandshakeInterceptor())
+                .withSockJS();
     }
 
 
@@ -28,23 +38,41 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     static class SubscriptionInterceptor implements ChannelInterceptor {
+        private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 
         @Override
         public Message<?> preSend(Message<?> message, MessageChannel channel) {
             var accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
             if (accessor != null && accessor.getDestination() != null & StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
-                System.err.println("## subscribe tenantid: " + accessor.getNativeHeader("X-TenantId"));
+                String tenantId = (String) accessor.getSessionAttributes().get("tenantId");
+                log.info("## subscribe to tenant: {}", tenantId);
 
                 if (accessor.getDestination().contains("/tenant/")) {
-                    if (!accessor.getDestination().equals("/tenant/" + TenantContext.getTenantId())) {
-                        System.err.println("Accesss to tenant denied");
+                    if (!accessor.getDestination().equals("/tenant/" + tenantId)) {
+                        log.error("Accesss to tenant denied: {}", accessor.getDestination());
                         throw new IllegalStateException("Accesss to tenant denied");
                     }
                 }
             }
 
             return message;
+        }
+    }
+
+    static class CustomHandshakeInterceptor implements HandshakeInterceptor {
+
+        @Override
+        public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+            String tenantId = request.getHeaders().getFirst("X-TenantId");
+            attributes.put("tenantId", tenantId);
+            return true;
+        }
+
+        @Override
+        public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
         }
     }
 }
